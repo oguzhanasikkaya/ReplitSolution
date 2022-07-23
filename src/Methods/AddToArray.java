475e0aff5c908f1e86ab451fffd5e2966909dbbe
oguzhanasikkaya <oguzhanasikkaya@yahoo.com>
2022-07-23 14:25:51 +0300

    add a number to array

---
+++ src/Methods/AddToArray.java
@@ -0,0 +1,37 @@
+package Methods;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddToArray {
+
+    public static void add_to_r(int[] r,int n) {
+        //create new array with one more position.
+        int[] result = new int[r.length+1];
+
+        for (int i = 0; i < r.length; i++) {
+            result[i]= r[i];
+        }
+        result[result.length-1] = n;
+
+        System.out.println(Arrays.toString(result));
+        //------------------------------------------
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int size = inp.nextInt(), n = inp.nextInt();
+        int[] arr = new int[size];
+        for(int i=0 ; i <=size-1; i++){
+            arr[i]=inp.nextInt();
+        }
+
+        add_to_r(arr, n);
+
+    }
+
+
+
+}
